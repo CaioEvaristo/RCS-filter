ffa1604432ec5adc0b00a4bfd340158f2868a2c1
Caio Evaristo <caio.evaristodesouza@hotmail.com>
2024-07-16 13:04:07 -0300

    remove mongo

--- rcs-filter-back/index.js
+++ rcs-filter-back/index.js
@@ -2,12 +2,10 @@ import 'dotenv/config'
 import express from 'express';
 import cors from 'cors';
 import router from './routes/index.js';
-import {connect} from './config/mongo.js';
 import enviroment from './config/enviroment.js';
 const app = express()
 
 async function main() {
-    await connect()
     app.use(cors());
     app.use(express.json());
     app.use(router);
